f6ce7cd0ad708e9345ae63511b67ea7fc3d2cb5a
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2023-10-25 02:08:24 -0700

    Revert "rce testing (#10)" (#13)
    This reverts commit d98b88b4b03750a9479ee5fee969d578d1ef04f6.
    
    The issue is that in practice I'm getting 403 unauthorized because
    bazel fails soft when the remote cache doesn't work.

--- ts/bazel/index.ts
+++
@@ -1,6 +0,0 @@
-import { isDefined, must } from 'ts/guard';
-export const workspaceDirectory = () =>
-	must(
-		isDefined,
-		() => 'BUILD_WORKING_DIRECTORY unspecified'
-	)(process.env['BUILD_WORKING_DIRECTORY']);
