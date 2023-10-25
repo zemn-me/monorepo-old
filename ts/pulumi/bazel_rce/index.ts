e56486bd50800942ed28d30c697622e941e900cc
zemnmez <zemnmez@googlemail.com>
2023-10-24 17:14:37 -0700

    Revert "crazy hack that just might work to fix https://github.com/pulumi/pulumi-github/issues/248\#issuecomment-1742415825"
    This reverts commit cd945102515a0837a88c85b0e4c8c38a4410fbee.

--- ts/pulumi/bazel_rce/index.ts
+++ ts/pulumi/bazel_rce/index.ts
@@ -46,7 +46,7 @@ interface DockerFileParams {
 }
 
 const password_file_name = '.htpasswd';
-const monorepo_github_name = '../zemn-me/monorepo';
+const monorepo_github_name = 'zemn-me/monorepo';
 
 function DockerFile(params: DockerFileParams) {
 	return `
