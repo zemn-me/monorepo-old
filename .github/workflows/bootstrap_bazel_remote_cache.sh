7a687a495ed004c86ebba1b51aec17c1ac9b57be
Thomas Neil James Shadwell <zemnmez@googlemail.com>
2023-10-27 02:56:42 -0700

    use buildbuddy (#21)

--- .github/workflows/bootstrap_bazel_remote_cache.sh
+++ .github/workflows/bootstrap_bazel_remote_cache.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
 # Dummy.
+
+
+
+echo "::group::Configure Bazel Remote Cache"
+
+if [[ -z "${BUILDBUDDY_API_KEY}" ]]; then
+	echo "::warning file=.github/workflows/bootstrap_bazel_remote_cache.sh,line=3,endLine=3,title=Running without bazel cache!:: BUILDBUDDY_API_KEY was not specified, so tests will be doing all the work from scratch."
+else
+	echo "Using bazel remote cache."
+	echo "build --remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}" > .auth.bazelrc
+fi
+
+echo "::endgroup::"
+
+exit 0
+
+
+
